c3b296af812fbf5b6f6b43c829db1930ff368768
vokS <danielcskov@gmail.com>
2022-05-02 13:42:38 +0200

    Add project files.

---
+++ Synkronisering øvelse/Øvelse 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace Synkronisering_øvelse
+{
+    class Program
+    {
+
+        static object countLock = new object();
+
+        static int count;
+
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(IncrementCount);
+            var t2 = new Thread(IncrementCount);
+
+            t1.Name = "Thread1";
+            t2.Name = "Thread2";
+
+            t1.Start();
+            Thread.Sleep(10);
+            t2.Start();
+
+        }
+
+        static void IncrementCount()
+        {
+            while (true)
+            {
+                lock (countLock)
+                {
+                    if (Thread.CurrentThread.Name == "Thread1")
+                    {
+                        count += 2;
+                        Console.WriteLine("{0} Count is currently at " + count, Thread.CurrentThread.Name);
+                    }
+                    if (Thread.CurrentThread.Name == "Thread2")
+                    {
+                        count--;
+                        Console.WriteLine("{0} Count is currently at " + count, Thread.CurrentThread.Name);
+                    }
+                }
+
+                if (Thread.CurrentThread.Name == "Thread1")
+                {
+                    Thread.Sleep(1000);
+                }
+
+                if (Thread.CurrentThread.Name == "Thread2")
+                {
+                    Thread.Sleep(2000);
+                }
+            }
+        }
+
+    }
+}
